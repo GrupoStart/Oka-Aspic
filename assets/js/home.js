a6d8748e7d2f9c27af408749400d2cb66fa29bd9
Ronaldo-Vieira-Lira <ronaldovieira.lira@gmail.com>
2021-10-11 01:59:56 -0300

    funcao reset, responsividade maps

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,28 +1,28 @@
-var botao = document.getElementById("menuBurg")
+var botao = document.getElementById("menuBurg");
 botao.addEventListener("click", () => {
     let trocaImg = document.getElementById("trocaImgs")
     let ulItens = document.getElementById("navegacao").classList.toggle("ativo")
     ulItens ? trocaImg.src = "assets/img/home/iconsHome/close.svg" : trocaImg.src = "assets/img/home/iconsHome/menu.svg" 
-})
+});
 
 /* Funções dos dois botões da área de chegada */
 
 document.getElementById("btnconhecaNos").addEventListener("click", () => {
     window.location = "#sobre"
-})
+});
 document.getElementById("btnlocalizacao").addEventListener("click", () => {
     window.location = "#localizacao"
-})
+});
 
 /* Função do botão que leva a pagina de melhores pratos */
 
 document.getElementById("btnFaleConosco").addEventListener("click", () => {
     window.location = "assets/pages/contatos.html"
-})
+});
 
 document.getElementById("butaoPopulares").addEventListener("click", () => {
     window.location = "assets/pages/populares.html"
-})
+});
 
 /* Função para ocultar o map */
 
@@ -37,4 +37,10 @@ document.getElementById("btnOcultar").addEventListener("click", () => {
         maps.style.display = "none"
         document.getElementById("btnOcultar").innerHTML = "Mostar Map <span id='iconMap'> <img src='assets/img/home/iconsHome/map.svg'> </span>"
     }
-})
+});
+
+/* Verificador caso o usuario saia com o tela de menu aberta  */
+window.addEventListener('resize', function (){
+	document.getElementById("navegacao").classList.remove("ativo")
+    document.getElementById("trocaImgs").src = "assets/img/home/iconsHome/menu.svg"
+}) 
